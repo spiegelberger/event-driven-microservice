ab0abaed17022b76bee13aa6034b2fc04f158e9e
Zsolt Spiegelberger <zsoltspiegelberger@gmail.com>
2021-06-10 20:51:37 +0200

    Added ProductsService and DiscoveryService

---
+++ DiscoveryServer/src/test/java/com/spiegelberger/estore/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spiegelberger.estore.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ ProductsService/src/main/java/com/spiegelberger/estore/ProductsService/rest/ProductsController.java
@@ -0,0 +1,35 @@
+package com.spiegelberger.estore.ProductsService.rest;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/products")
+public class ProductsController {
+
+	
+	@PostMapping
+	public String createProduct() {
+		return "HTTP POST Handled";
+	}
+	
+	@GetMapping
+	public String getProduct() {
+		return "HTTP GET Handled";
+	}
+	
+	@PutMapping
+	public String updateProduct() {
+		return "HTTP PUT Handled";
+	}
+	
+	@DeleteMapping
+	public String deleteProduct() {
+		return "HTTP DELETE Handled";
+	}
+}
